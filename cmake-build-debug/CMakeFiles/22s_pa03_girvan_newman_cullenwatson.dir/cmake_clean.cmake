994c94d4e3cebf7910ce0e33933638b3f29d2bcd
cgwatson193 <78247585+cgwatson193@users.noreply.github.com>
2022-03-28 11:20:32 -0500

    proj made

---
+++ cmake-build-debug/CMakeFiles/22s_pa03_girvan_newman_cullenwatson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "22s_pa03_girvan_newman_cullenwatson"
+  "22s_pa03_girvan_newman_cullenwatson.pdb"
+  "CMakeFiles/22s_pa03_girvan_newman_cullenwatson.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22s_pa03_girvan_newman_cullenwatson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
