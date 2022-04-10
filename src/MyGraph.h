623fae7201c85aeb90046805f43757f90511802a
Cullen Watson <78247585+cgwatson193@users.noreply.github.com>
2022-04-09 19:20:44 -0500

    Update MyGraph.h

--- src/MyGraph.h
+++ src/MyGraph.h
@@ -6,24 +6,31 @@
 #include <utility>
 #include <algorithm>
 #include <vector>
+#include <stack>
+#include <queue>
 #include "boost/graph/graph_traits.hpp"
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/graphml.hpp"
+#include "boost/graph/betweenness_centrality.hpp"
 
+#include <boost/test/minimal.hpp>
+#include <boost/random/uniform_01.hpp>
+#include <boost/random/linear_congruential.hpp>
+#include <boost/lexical_cast.hpp>
 using namespace boost;
 using namespace std;
-typedef adjacency_list<vecS, vecS, undirectedS> UndirectedGraph;
-typedef graph_traits<UndirectedGraph>::edge_iterator edge_iterator;
+typedef adjacency_list<vecS, vecS, undirectedS> Graph;
+typedef graph_traits<Graph>::edge_iterator edge_iterator;
 
 class MyGraph {
 private:
-    UndirectedGraph g;
+    Graph g;
 public:
     // reads in graphml file
     MyGraph(string);
 
     void outputEdges();
-
+    void computeBetweenness();
 };
 
 
